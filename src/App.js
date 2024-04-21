b7dc35a27909a80a0660338d27e49edfbcadd76a
Amilton Castro <amiltonscjunior@gmail.com>
2024-04-21 14:43:20 +0100

    add Navbar and landingPage components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useEffect } from 'react';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './Components/Navbar/navbar';
+import Layout from './Components/Landing_Page/landingPage';
+import LandingPage from './Components/Landing_Page/landingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Navbar/navbar.js
@@ -0,0 +1,43 @@
+import './navbar.css'
+
+function Navbar() {
+  return (
+
+    
+    <nav>
+    <div className="nav__logo">
+      <a href="/">
+      StayHealthy </a>
+    </div>
+    <div className="nav__icon" >
+      <i className="fa fa-times fa fa-bars"></i>
+    </div>
+    <ul className="nav__links active">
+      <li className="link">
+        <a href="/">Home</a>
+      </li>
+      <li className="link">
+        <a href="#">Appointments</a>
+      </li>
+      <li className="link">
+        <a href="#">Health Blog</a>
+      </li>
+      <li className="link">
+        <a href="#">Reviews</a>
+      </li>
+      <li className="link">
+        <a href="../Sign_up/sign_up.html">
+          <button className="btn1">Sign Up</button>
+        </a>
+      </li>
+      <li className="link">
+        <a href="../Login/login.html">
+          <button className="btn1">Login</button>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  )
+}
+
+export default Navbar
