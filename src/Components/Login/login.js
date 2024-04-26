f27d52be4c5b77e33de31c3612f937359b3f5abe
Amilton Castro <amiltonscjunior@gmail.com>
2024-04-26 20:56:20 +0100

    setting signIn and Login files

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,8 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './login.css';
+import { API_URL } from '../../config';
 
-function Login() {
+
+const Login = () => {
+
+  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('');
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (sessionStorage.getItem("auth-token")) {
+            navigate("/")
+        }
+    }, []);
+
+    const login = async (e) => {
+        e.preventDefault();
+        const res = await fetch(`${API_URL}/api/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                // name: name,
+                email: email,
+                password: password,
+            }),
+        });
+
+        const json = await res.json();
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+
+            sessionStorage.setItem('email', email);
+
+            navigate('/');
+            window.location.reload()
+        } else {
+            if (json.errors) {
+                for (const error of json.errors) {
+                    alert(error.msg);
+                }
+            } else {
+                alert(json.error);
+            }
+        }
+    };
+
   return (
     <div className="container">
         <div className="login-grid">
@@ -14,14 +60,16 @@ function Login() {
           </div>
           <br />
           <div className="login-form">
-            <form>
+            <form onSubmit={login}>
               <div className="form-group">
                         <label for="email">Email</label>
-                        <input type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
                     </div>
               <div className="form-group">
-                <label for="password" >Password</label>
+                <label htmlFor="password" >Password</label>
                 <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   type="password"
                   name="password"
                   id="password"
--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,42 +1,123 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import './navbar.css'
 
-function Navbar() {
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("auth-token");
+    sessionStorage.removeItem("name");
+    sessionStorage.removeItem("email");
+    sessionStorage.removeItem("phone");
+    // remove email phone
+    localStorage.removeItem("doctorData");
+    setIsLoggedIn(false);
+    // setUsername("");
+
+    // Remove the reviewFormData from local storage
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key.startsWith("reviewFormData_")) {
+            localStorage.removeItem(key);
+        }
+    }
+    setEmail('');
+    window.location.reload();
+  }
+
+  const handleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  }
+  useEffect(() => {
+    const storedemail = sessionStorage.getItem("email");
+
+    if (storedemail) {
+        setIsLoggedIn(true);
+        setUsername(storedemail);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Check if the user is already logged in
+    const storedUsername = sessionStorage.getItem("name");
+
+    if (storedUsername) {
+        setIsLoggedIn(true);
+        setUsername(storedUsername);
+    }
+  }, []);
+
   return (
 
     
     <nav>
-    <div className="nav__logo">
-      <a href="/">
-      StayHealthy </a>
-    </div>
-    <div className="nav__icon" >
-      <i className="fa fa-times fa fa-bars"></i>
-    </div>
-    <ul className="nav__links active">
-      <li className="link">
-        <a href="/">Home</a>
-      </li>
-      <li className="link">
-        <a href="#">Appointments</a>
-      </li>
-      <li className="link">
-        <a href="#">Health Blog</a>
-      </li>
-      <li className="link">
-        <a href="#">Reviews</a>
-      </li>
-      <li className="link">
-        <a href="../Sign_up/sign_up">
-          <button className="btn1">Sign Up</button>
-        </a>
-      </li>
-      <li className="link">
-        <a href="../Login/login">
-          <button className="btn1">Login</button>
-        </a>
-      </li>
-    </ul>
-  </nav>
+      <div className="nav__logo">
+        <a href="/">
+        StayHealthy </a>
+      </div>
+      <div className="nav__icon" onClick={handleClick}>
+        <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+      </div>
+      <ul className={click ? 'nav__links active' : 'nav__links'}>
+        <li className="link">
+          <a href="/">Home</a>
+        </li>
+        <li className="link">
+          <a href="#">Appointments</a>
+        </li>
+        <li className="link">
+          <a href="#">Health Blog</a>
+        </li>
+        <li className="link">
+          <a href="#">Reviews</a>
+        </li>
+        {isLoggedIn ? (
+          <>
+            <li onClick={handleDropdown} className="link welcome-user">
+                <p>
+                    Welcome, {username}
+                </p>
+                {showDropdown && (
+                    <ul className="dropdown-menu">
+                        <li>
+                            <Link to="/profile">Your Profile</Link>
+                        </li>
+                        <li>
+                            <Link to="/reports">Your Reports</Link>
+                        </li>
+                    </ul>
+                )}
+            </li>
+            <li className="link">
+                <button className="btn2" onClick={handleLogout}>
+                    Logout
+                </button>
+            </li>
+
+          </>
+
+        ) : (
+          <> 
+            <li className="link">
+              <a href="../Sign_up/sign_up">
+                <button className="btn1">Sign Up</button>
+              </a>
+            </li>
+            <li className="link">
+              <a href="../Login/login">
+                <button className="btn1">Login</button>
+              </a>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
   )
 }
 
